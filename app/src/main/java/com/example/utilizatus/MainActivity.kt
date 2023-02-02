3201e99ad977c888c30d0427de8733b0a9aeb2bc
Artorius <Artorius.81@yandex.ru>
2023-02-02 19:22:08 +1000

    Project created

---
+++ app/src/main/java/com/example/utilizatus/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.utilizatus
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.utilizatus.ui.theme.UtilizatusTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.splashScreenTheme)
+        super.onCreate(savedInstanceState)
+        setContent {
+            UtilizatusTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    UtilizatusTheme {
+        Greeting("Android")
+    }
+}
